cd57ee29819e6508317bdfcbe3aa20b3c429ca2a
wobsoriano <sorianorobertc@gmail.com>
2022-11-23 11:30:25 -0800

    update test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,19 @@
-import { readFile } from 'fs/promises'
 import { expect, test } from 'vitest'
-import { transformMarkdown } from '../src'
+import { removeExports } from '../src'
+
+const code = `
+import { readFileSync } from 'fs'
+
+export const getServerSideProps = () => {
+  return {
+    content: readFileSync('./foo.txt', 'utf-8'),
+  }
+}
+
+console.log("hello world")
+`
 
 test('Correct markdown to tree-shakable ES Module imports', async () => {
-  const content = await readFile('test/content.md', 'utf-8')
-  const result = transformMarkdown(content)
-  expect(result).toMatchSnapshot()
+  const result = removeExports(code, ['getServerSideProps'])
+  expect(result?.code).toBe('console.log("hello world");')
 })
