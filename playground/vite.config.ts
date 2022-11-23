e1f5001c97e1a421d783c67592e932fd62593aa3
wobsoriano <sorianorobertc@gmail.com>
2022-11-23 11:56:34 -0800

    update playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import StripExports from 'unplugin-strip-exports/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    StripExports({
+      match(filepath: string, ssr) {
+        // Ignore SSR build
+        if (ssr)
+          return
+
+        if (filepath.includes('tsx'))
+          return ['getServerSideProps']
+      },
+    }),
+    react(),
+  ],
 })
