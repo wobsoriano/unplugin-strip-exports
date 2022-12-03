96df16f841efbbc1266a3a5fdd849fc6dfe19d1c
wobsoriano <sorianorobertc@gmail.com>
2022-12-03 15:28:00 -0800

    fix: remove unused option type

--- src/types.ts
+++ src/types.ts
@@ -5,10 +5,7 @@ type MatchFunction = (
   ssr?: boolean,
 ) => string[] | undefined | null | void
 
-type BeforeOuput = (code: string) => string
-
 export interface Options {
   match: MatchFunction
   babelTransformOptions?: TransformOptions
-  beforeOutput?: BeforeOuput
 }
